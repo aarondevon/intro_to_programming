6f0af70c1e44e27f57dd35583242c21df50c6624
aarondevon <me@aarondevon.com>
2016-09-27 20:37:25 -0500

    complete variable exercises

---
+++ b_variables/name.rb
@@ -0,0 +1,9 @@
+# Exercises 1, 3, and 4.
+puts "Please input your first name..."
+first_name = gets.chomp
+puts "Now, please input your last name..."
+last_name = gets.chomp
+puts "Greetings #{first_name} #{last_name}, we hope your visit is mediocre."
+
+
+10.times {|n| puts "#{first_name} #{last_name}" }
