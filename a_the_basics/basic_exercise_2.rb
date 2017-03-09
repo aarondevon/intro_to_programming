820a7f5fad37d30563b5b14f4f7ad4bd2def1826
aarondevon <me@aarondevon.com>
2017-03-09 08:11:29 -0600

    Find the thousands, hundreds, tens, and ones place in a number

---
+++ a_the_basics/basic_exercise_2.rb
@@ -0,0 +1,14 @@
+# Use the modulo operator, division, or a combination of both to take a 4
+# digit number and find the digit in the:
+
+# 1) thousands place
+puts 9356 / 1000
+
+# 2) hundreds place
+puts 9356 % 1000 / 100
+
+# 3) tens place
+puts 9356 % 1000 % 100 / 10
+
+# 4) ones place
+puts 9356 % 1000 % 100 % 10 /1
