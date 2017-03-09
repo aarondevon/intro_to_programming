db3f8e8d2755170db46de5a1ef4832d038740205
aarondevon <me@aarondevon.com>
2017-03-09 07:59:02 -0600

    Update example

--- a_the_basics/basic_exercise_1.rb
+++ a_the_basics/basic_exercise_1.rb
@@ -1,4 +1,11 @@
 # Add two strings together that, when concatenated, return your first and
 # last name as your full name in one string.
 
-"Aaron " + "Sawyer"
+first_name = 'Aaron'
+last_name = 'Sawyer'
+
+puts first_name + ' ' + last_name
+
+# or
+
+puts " #{first_name} #{last_name}"
