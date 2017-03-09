1ad552b39908c85b074f46d5541d9cad46965b19
aarondevon <me@aarondevon.com>
2017-03-09 08:33:43 -0600

    Use a hash to store movie titles and year they came out then print to year to screen

---
+++ a_the_basics/basic_exercise_3.rb
@@ -0,0 +1,12 @@
+# Write a program that uses a hash to store a list of movie titles with the
+# year they came out. Then use the puts command to make your program print out
+# the year of each movie to the screen.
+
+movies = {willow: 1988, the_princess_bride: 1987, the_legend_of_johnny_lingo: 2003, wall_e: 2008, how_to_train_your_dragon: 2010, big_hero_6: 2014}
+
+puts [movies[:willow]]
+puts [movies[:the_princess_bride]]
+puts [movies[:the_legend_of_johnny_lingo]]
+puts [movies[:wall_e]]
+puts [movies[:how_to_train_your_dragon]]
+puts [movies[:big_hero_6]]
