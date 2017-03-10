4a07fa8b86d8ff631986d4b4de676c624288801f
aarondevon <me@aarondevon.com>
2017-03-10 08:17:45 -0600

    Rewrite method from exercise 4 so it prints

---
+++ c_methods/method_exercise_5.rb
@@ -0,0 +1,8 @@
+# 1) Edit the method in exercise #4 so that it does print words on the screen.
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+# 2) What does it return now?
+screem("Yippeee") # => nil because this what #puts returns
