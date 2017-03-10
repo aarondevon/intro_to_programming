fc231b465c7a23805ee1e1a95114b98754836017
aarondevon <me@aarondevon.com>
2017-03-10 07:49:46 -0600

    Evaluate expressions

---
+++ c_methods/method_exercise_2.rb
@@ -0,0 +1,11 @@
+# What do the following expressions evaluate to?
+
+x = 2 # => 2
+
+puts x = 2 # It prints 2 then => nil
+
+p name = "Joe" # It prints "Joe" then => "Joe"
+
+four = "four" # => "four"
+
+print something = "nothing" # It prints nothing with no line break then => nil
