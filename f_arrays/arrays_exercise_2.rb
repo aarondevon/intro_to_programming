0a34a17a13ac2cf0ef38ac14734355d95e727fb6
aarondevon <me@aarondevon.com>
2017-03-14 08:25:53 -0500

    What will the programs return and what is the value of arr

--- f_arrays/arrays_exercise_2.rb
+++ f_arrays/arrays_exercise_2.rb
@@ -4,3 +4,9 @@
 arr = ["b", "a"]
 arr = arr.product(Array(1..3))
 arr.first.delete(arr.first.last)
+# returns 1 and arr is now [["b"], ["b", 2], ["b", 3], ["a", 1], ["a", 2], ["a", 3]]
+
+arr = ["b", "a"]
+arr = arr.product([Array(1..3)])
+arr.first.delete(arr.first.last)
+# returns [1, 2, 3] arr is now [["b"], ["a", [1, 2, 3]]]
