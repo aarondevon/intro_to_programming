d3191da5c34fc66f742d1229787aafca7007e69a
aarondevon <me@aarondevon.com>
2017-03-23 08:10:43 -0500

    Assign array to a variable

--- j_exercises/exercises_exercise_3.rb
+++ j_exercises/exercises_exercise_3.rb
@@ -1,7 +1,7 @@
 # Now, using the same array from #2, use the select method to extract all
 # odd numbers into a new array.
-
-odd_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].select do |number|
+number_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+odd_array = number_array.select do |number|
   number % 2 != 0
 end
 
