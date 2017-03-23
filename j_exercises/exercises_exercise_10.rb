2ee15faef1240b7934ae45e52217ed9445718903
aarondevon <me@aarondevon.com>
2017-03-23 14:20:53 -0500

    Can hash values be arrays? Can you have an array of hashes

---
+++ j_exercises/exercises_exercise_10.rb
@@ -0,0 +1,8 @@
+# 10) Can hash values be arrays? Can you have an array of hashes?
+# (give examples)
+
+example1 = {dogs: ["Siberian Husky", "Labrador Retriever", "German Shepherd"] }
+p example1
+
+example2 = [{david_age: 9}, {hyrum_age: 6}, owen_age: 4]
+p example2
